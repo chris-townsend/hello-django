67957631376076d9f90548813d6692d6c612d8d1
chris-townsend <chris-townsend@live.co.uk>
2022-10-26 10:18:11 +0000

    Add form model

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 
@@ -11,6 +12,16 @@ def get_todo_list(request):
     }
     return render(request, 'todo/todo_list.html', context)
 
+
 def add_item(request):
-    
-    return render(request, 'todo/add_item.html')
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form': form
+        }
+
+    return render(request, 'todo/add_item.html', context)
