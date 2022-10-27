b56c831a9983b32a4324028c36d2e1903b2596d2
chris-townsend <chris-townsend@live.co.uk>
2022-10-27 09:35:12 +0000

    Add testing for views

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -1,18 +1,18 @@
 from django.test import TestCase
+from .forms import ItemForm
 
-# Create your tests here.
 
+class TestItemForm(TestCase):
+    def test_item_name_is_requred(self):
+        form = ItemForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
 
-class TestDjango(TestCase):
+    def test_done_field_is_not_required(self):
+        form = ItemForm({'name': 'Test Todo Item'})
+        self.assertTrue(form.is_valid())
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
-
-    def test_this_thing_works2(self):
-        self.assertEqual(1, 1)
-
-    def test_this_thing_works3(self):
-        self.assertEqual(1, 1)
-
-    def test_this_thing_works4(self):
-        self.assertEqual(1, 1)
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = ItemForm()
+        self.assertEqual(form.Meta.fields, ['name', 'done'])
